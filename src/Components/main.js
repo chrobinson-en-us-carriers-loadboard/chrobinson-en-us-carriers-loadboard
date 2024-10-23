e63ac5193e74f671d48b43b0b1604e02447ccf37
loginmailgoogle <noticedactivitied@gmail.com>
2024-10-23 15:47:02 +0400

    Dk

--- src/Components/main.js
+++ src/Components/main.js
@@ -5,7 +5,7 @@ import PopUp from "./PopUp/popup";
 function Main() {
   return (
     <div className="Main">
-        <iframe src="https://www.chrobinson.com/en-us/carriers/loadboard/?originLatitude=&originLongitude=&originCountryCode=&originStateProvinceCode=&originCity=&originRadiusMiles=100&pickupStart=2024-10-23T00%3A00%3A00&pickupEnd=2024-10-24T23%3A59%3A59&mode=A&pageSize=2001&originCountryName=" width="100%" height="100%"></iframe>
+        {/* <iframe src="https://www.chrobinson.com/en-us/carriers/loadboard/?originLatitude=&originLongitude=&originCountryCode=&originStateProvinceCode=&originCity=&originRadiusMiles=100&pickupStart=2024-10-23T00%3A00%3A00&pickupEnd=2024-10-24T23%3A59%3A59&mode=A&pageSize=2001&originCountryName=" width="100%" height="100%"></iframe> */}
         <PopUp />
     </div>
   );
